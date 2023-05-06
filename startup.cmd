85956a5295d00f639dfd3f7818b1e2a394f2769f
YongBinnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn <127243543+YongBinnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2023-05-06 11:36:01 -0700

    startup.cmd: auto run as admin

--- startup.cmd
+++ startup.cmd
@@ -1,4 +1,8 @@
-%~dp0%\third_party\pssuspend64.exe wlms.exe
+@echo off
+%~dp0%\third_party\pssuspend64.exe wlms.exe || (
+    powershell.exe -noprofile -c Start-Process -Verb RunAs cmd.exe \"/c %0 1\"
+    exit
+)
 taskkill /f /im msteams.exe
 echo taskkill /f /im widget.exe
 pause
