3c8ec5392b9d73de5388974fa5460e1750ed6460
Bin Yong <yongb@usi.ch>
2024-09-22 04:50:19 +0800

    Update xwim.cmd

--- xwim.cmd
+++ xwim.cmd
@@ -11,6 +11,8 @@ mkdir %temp%\wye
 expand third_party\Drivers\Microsoft\Apple_Tether_USB_Drivers_netaapl_7503681835e08ce761c52858949731761e1fa5a1.cab -F:* %temp%\wye
 expand third_party\Drivers\Microsoft\Apple_USB_Drivers_01d96dfd-2f6f-46f7-8bc3-fd82088996d2_a31ff7000e504855b3fa124bf27b3fe5bc4d0893.cab -F:* %temp%\wye
 
+copy third_party\Microsoft\WIndows\System32\* %temp%\wyem\WIndows\System32\
+
 dism /image:%tmp%\wyem /add-driver /driver:%temp%\wye\netaapl64.inf
 dism /image:%tmp%\wyem /add-driver /driver:%temp%\wye\AppleUsb.inf
 dism /unmount-image /mountdir:%tmp%\wyem /commit
