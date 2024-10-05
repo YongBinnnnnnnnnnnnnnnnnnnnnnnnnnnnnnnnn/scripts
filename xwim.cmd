bbfff9721350a5049a7e5dcedbf60dfc0edfc1df
Bin Yong <yongbin@disroot.org>
2024-10-04 22:17:53 -0700

    Add files via upload

--- xwim.cmd
+++ xwim.cmd
@@ -5,14 +5,15 @@ setlocal enabledelayedexpansion
 mkdir %temp%\wyem
 dism /mount-wim /wimfile:boot.wim /mountdir:%temp%\wyem /index:1
 
-ren %temp%\wyem\WIndows\System32\drivers\acpiex.sys acpiex.sys.ybkup
-copy /Y hbcd_SYSTEM %tmp%\wyem\WIndows\System32\SYSTEM
+::ren %temp%\wyem\WIndows\System32\drivers\acpiex.sys acpiex.sys.ybkup
+::copy %temp%\wyem\WIndows\System32\drivers\beep.sys %temp%\wyem\WIndows\System32\drivers\acpiex.sys
+set /p wim_source="wim source:"
+copy /Y hbcd_SYSTEM %temp%\wyem\Windows\System32\config\SYSTEM
 mkdir %temp%\wye
 expand third_party\Drivers\Microsoft\Apple_Tether_USB_Drivers_netaapl_7503681835e08ce761c52858949731761e1fa5a1.cab -F:* %temp%\wye
-expand third_party\Drivers\Microsoft\Apple_USB_Drivers_01d96dfd-2f6f-46f7-8bc3-fd82088996d2_a31ff7000e504855b3fa124bf27b3fe5bc4d0893.cab -F:* %temp%\wye
 
 copy third_party\Microsoft\WIndows\System32\* %temp%\wyem\WIndows\System32\
 
-dism /image:%tmp%\wyem /add-driver /driver:%temp%\wye\netaapl64.inf
-dism /image:%tmp%\wyem /add-driver /driver:%temp%\wye\AppleUsb.inf
-dism /unmount-image /mountdir:%tmp%\wyem /commit
+dism /image:%temp%\wyem /add-driver /driver:%temp%\wye\netaapl64.inf
+dism /image:%temp%\wyem /add-driver /driver:third_party\Drivers\Apple\usbaapl64.inf
+dism /unmount-image /mountdir:%temp%\wyem /commit
