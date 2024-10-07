67efabda1b2269f0038d6946ef140e8cc1bf2a32
Bin Yong <yongbin@disroot.org>
2024-10-07 09:03:35 -0700

    Add files via upload

--- xwim.cmd
+++ xwim.cmd
@@ -12,9 +12,23 @@ copy /Y hbcd_SYSTEM %temp%\wyem\Windows\System32\config\SYSTEM
 mkdir %temp%\wye
 expand third_party\Drivers\Microsoft\Apple_Tether_USB_Drivers_netaapl_7503681835e08ce761c52858949731761e1fa5a1.cab -F:* %temp%\wye
 
-copy third_party\Microsoft\WIndows\System32\* %temp%\wyem\WIndows\System32\
+copy third_party\Microsoft\Windows\System32\* %temp%\wyem\Windows\System32\
+
+del %temp%\wyem\Windows\System32\drivers\fltsrv.sys
+del %temp%\wyem\Windows\System32\drivers\snapman.sys
+del %temp%\wyem\Windows\System32\drivers\volume_tracker.sys
+del %temp%\wyem\Windows\System32\drivers\EUDCPEPM.sys
+del %temp%\wyem\Windows\System32\drivers\EUEDKEPM.sys
+del %temp%\wyem\Windows\System32\drivers\ambakdrv.sys
+del %temp%\wyem\Windows\System32\drivers\amwrtdrv10.sys
+del %temp%\wyem\Windows\System32\drivers\ammntdrv10.sys
+
 
 dism /image:%temp%\wyem /add-driver /driver:%temp%\wye\netaapl64.inf
+dism /image:%temp%\wyem /add-driver /driver:%temp%\wye\netaapl64.inf
 dism /image:%temp%\wyem /add-driver /driver:third_party\Drivers\Apple\usbaapl64.inf
-dism /image:%temp%\wyem /add-driver /driver:third_party\Drivers\Microsoft\usbncm.inf_amd64_4b17399b45eb003f\usbncm.inf
+dism /image:%temp%\wyem /add-driver /driver:..\u0407010.inf_amd64_3cdbf86c96072d50\u0407010.inf
+
+dism /image:%temp%\wyem /remove-driver /driver:%temp%\wyem\Windows\INF\oem269.inf
+
 dism /unmount-image /mountdir:%temp%\wyem /commit
